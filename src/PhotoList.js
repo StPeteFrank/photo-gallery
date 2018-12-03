ac6daa10c5b8d246d373da98ccf81b12487ba0f6
Frank Diaz <diazfrankp@gmail.com>
2018-12-03 18:37:33 -0500

    Some css

--- src/PhotoList.js
+++ src/PhotoList.js
@@ -13,9 +13,9 @@ class PhotoList extends Component {
         {categoryData.photos.map((photo, index) => {
           return (
             <li key={index}>
-              <p>{photo.title}</p>
-
               <Link to={`/${category}/${index}`}>
+                <p>{photo.title}</p>
+
                 <img src={photo.imageURL} />
               </Link>
             </li>
