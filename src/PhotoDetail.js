b014dafe9c06e959ffe6b73d1a06be08717dba5d
Frank Diaz <diazfrankp@gmail.com>
2018-12-03 16:36:07 -0500

    PhotoDetail is dynamic using categoryData and photoIndex.

--- src/PhotoDetail.js
+++ src/PhotoDetail.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react'
 
+import samples from './samples.json'
+
 class PhotoDetail extends Component {
   render() {
+    const categoryData = samples['pandas']
+    const photoIndex = 0
+
     return (
       <div>
-        <h2>Panda Waiving</h2>
-        <img src="https://cdn-images-1.medium.com/max/1600/1*i1vVm3EqqDIkyucD0079wg.jpeg" />
+        <h2>{categoryData.photos[photoIndex].title}</h2>
+        <img src={categoryData.photos[photoIndex].imageURL} />
       </div>
     )
   }
