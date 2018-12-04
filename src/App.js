c22ee7cd6c7ecda89203d48151e2e97ee558aa3c
Frank Diaz <diazfrankp@gmail.com>
2018-12-03 19:59:41 -0500

    CSS updated gradient.

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
   render() {
     return (
       <Router>
-        <div>
+        <div className="app">
           <Route exact path="/" component={CategoryList} />
           <Route exact path="/:category" component={PhotoList} />
           <Route path="/:category/:index" component={PhotoDetail} />
